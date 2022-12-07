a1e5d281365b493ac851790dd06c794d4a76051b
wesse206 <66951387+wesse206@users.noreply.github.com>
2022-12-07 13:50:58 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [HomeComponent, HeaderComponent],
+  bootstrap: [HeaderComponent, HomeComponent],
 })
-export class AppModule { }
+export class AppModule {}
