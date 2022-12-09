08884acc4018175f5152c38a9ab45d4866a9db20
wesse206 <66951387+wesse206@users.noreply.github.com>
2022-12-09 07:31:58 +0200

    3 files modified, 10 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { HomeComponent } from './home.component';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ShopComponent } from './shop/shop.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [HomeComponent, HeaderComponent],
-  bootstrap: [HeaderComponent, HomeComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, HeaderComponent, ShopComponent, HomeComponent],
+  bootstrap: [HeaderComponent, AppComponent],
 })
 export class AppModule {}
